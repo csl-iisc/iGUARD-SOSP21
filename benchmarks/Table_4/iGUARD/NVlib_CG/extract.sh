cff19670cf53dbc6389f432fe585db119fad4481
Aditya K Kamath <akamath1997@gmail.com>
2021-08-18 19:49:22 +0530

    Add code for NVlib_CG

--- benchmarks/Table_4/iGUARD/NVlib_CG/extract.sh
+++ benchmarks/Table_4/iGUARD/NVlib_CG/extract.sh
@@ -3,23 +3,12 @@ PREFIX=$1
 
 # grid_sync
 benchmark="grid_sync"
-caught[0]=0;
-races=$(grep -A1 "Race" "${PREFIX}${benchmark}.raw" | grep -oE "Line [0-9]+" | grep -oE "[0-9]+")
+caught=0;
+races=$(grep -A1 "Race" "${PREFIX}${benchmark}.raw")
+races=$(echo $races | grep -oE "Missing [a-z]+fence")
+# Races are caused by missing fence
 for i in $races; do
-	if [ $i == "168" ]; then
-		caught[0]=1;
-	elif [ $i == "215" ]; then
-		caught[0]=1;
-	elif [ $i == "222" ]; then
-		caught[0]=1;
-	elif [ $i == "266" ]; then
-		caught[0]=1;
-	fi
+	caught=1;
 done
 
-total=0
-for i in ${caught[@]}; do
-	total=$(awk "BEGIN {print $total + $i}");
-done
-
-echo ${benchmark} ${total}
+echo ${benchmark} ${caught}

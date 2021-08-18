78b324b16e4881432b64c78c91963f1b69c61c24
Aditya K Kamath <akamath1997@gmail.com>
2021-08-18 22:54:13 +0530

    Clean up Figure 10 output and standardize to match figure further

--- benchmarks/Figure_10/extract.sh
+++ benchmarks/Figure_10/extract.sh
@@ -4,6 +4,7 @@ PREFIX_NODET=$1
 PREFIX_DET1=$2
 PREFIX_DET2=$3
 RUNS=$4
+BENCHMARKS=$5
 declare -A nodet_res
 declare -A det1_res
 declare -A det2_res
@@ -35,29 +36,9 @@ for runs in `seq 1 1 ${RUNS}`; do
 	done
 done
 
-: '''
-for i in ${!nodet_res[@]}; do
-	echo $i
-	nodet_res[$i]=$(awk "BEGIN {print ${nodet_res[$i]}/${RUNS}}")
-	echo ${nodet_res[$i]}
-done
-
-for i in ${!det1_res[@]}; do
-	echo $i
-	det1_res[$i]=$(awk "BEGIN {print ${det1_res[$i]}/${RUNS}}")
-	echo ${det1_res[$i]}
-done
-
-for i in ${!det2_res[@]}; do
-	echo $i
-	det2_res[$i]=$(awk "BEGIN {print ${det2_res[$i]}/${RUNS}}")
-	echo ${det2_res[$i]}
-done
-'''
-
 printf "Final results\n"
 printf "Benchmark\tBaseline\tWith optimization\n"
-for i in ${!nodet_res[@]}; do
+for i in ${BENCHMARKS}; do
 	with_opt=$(awk "BEGIN {print ${det1_res[$i]}/${nodet_res[$i]}}")
 	without_opt=$(awk "BEGIN {print ${det2_res[$i]}/${nodet_res[$i]}}")
 	printf "%s\t%f\t%f\n" $i $without_opt $with_opt
